d00e16b947d6592cba33d36f2fb841b00f56fd03
Tulir293 <tulir293@maunium.net>
2016-06-14 23:08:36 +0300

    Added util.go with GenerateNonce()

---
+++ util.go
@@ -0,0 +1,33 @@
+package mautrix
+
+import (
+	"math/rand"
+	"time"
+)
+
+const nonceAC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+const (
+	letterIdxBits = 6
+	letterIdxMask = 1<<letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+// GenerateNonce generates a random string
+func GenerateNonce() string {
+	b := make([]byte, 32)
+	for i, cache, remain := 4, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(nonceAC) {
+			b[i] = nonceAC[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
