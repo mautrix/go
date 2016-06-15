dc6f0e3586b00078c555febbce201a12fae108bc
Tulir293 <tulir293@maunium.net>
2016-06-15 09:35:12 +0300

    Proper SendToRoom() response parsing

--- room.go
+++ room.go
@@ -11,6 +11,13 @@ type RoomInfo struct {
 	Name string
 }
 
+// SendResponse wraps the response to a room send request
+type SendResponse struct {
+	EventID   string `json:"event_id"`
+	Error     string `json:"error"`
+	ErrorCode string `json:"errcode"`
+}
+
 // RoomNameToID - get the room id from a room name
 func (session *Session) RoomNameToID(roomName string) string {
 	for k, v := range session.Rooms {
@@ -35,19 +42,14 @@ func (session *Session) SendToRoom(room, message string) error {
 		return err
 	}
 
-	var data map[string]interface{}
+	var data SendResponse
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
 		return err
+	} else if len(data.Error) > 0 {
+		return fmt.Errorf(data.Error)
+	} else if len(data.EventID) == 0 {
+		return fmt.Errorf("No event ID received!")
 	}
-	errStr, ok := data["error"]
-	if ok {
-		return fmt.Errorf(errStr.(string))
-	}
-
-	// fmt.Println(data)
-
-	// TODO: Check that the response is what we expected
-
 	return nil
 }
