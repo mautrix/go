69b0fcb79d146f2e0538a8c980f20f51f97369e6
Kegan Dougal <kegan@matrix.org>
2016-12-01 17:32:16 +0000

    Add LeaveRoom(roomID) with test

--- client_test.go
+++ client_test.go
@@ -1,6 +1,12 @@
 package gomatrix
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
 
 func ExampleClient_BuildURLWithQuery() {
 	cli, _ := NewClient("https://matrix.org", "@example:matrix.org", "abcdef123456")
@@ -25,4 +31,40 @@ func ExampleClient_BuildBaseURL() {
 	out := cli.BuildBaseURL("_matrix", "client", "r0", "directory", "room", "#matrix:matrix.org")
 	fmt.Println(out)
 	// Output: https://matrix.org/_matrix/client/r0/directory/room/%23matrix:matrix.org?access_token=abcdef123456
+}
+
+func TestClient_LeaveRoom(t *testing.T) {
+	cli := mockClient(func(req *http.Request) (*http.Response, error) {
+		if req.Method == "POST" && req.URL.Path == "/_matrix/client/r0/rooms/!foo:bar/leave" {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(`{}`)),
+			}, nil
+		}
+		return nil, fmt.Errorf("unhandled URL: %s", req.URL.Path)
+	})
+
+	if _, err := cli.LeaveRoom("!foo:bar"); err != nil {
+		t.Fatalf("LeaveRoom: error, got %s", err.Error())
+	}
+}
+
+func mockClient(fn func(*http.Request) (*http.Response, error)) *Client {
+	mrt := MockRoundTripper{
+		RT: fn,
+	}
+
+	cli, _ := NewClient("https://test.gomatrix.org", "@user:test.gomatrix.org", "abcdef")
+	cli.Client = &http.Client{
+		Transport: mrt,
+	}
+	return cli
+}
+
+type MockRoundTripper struct {
+	RT func(*http.Request) (*http.Response, error)
+}
+
+func (t MockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	return t.RT(req)
 }
