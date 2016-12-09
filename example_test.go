e989121b7a362fbd4bc35587210a513c52e9a870
Kegan Dougal <kegan@matrix.org>
2016-12-09 16:16:45 +0000

    Sync examples

--- example_test.go
+++ example_test.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 )
 
-func Example() {
+func Example_blockingSync() {
 	cli, _ := NewClient("https://matrix.org", "@example:matrix.org", "MDAefhiuwehfuiwe")
 	syncer := cli.Syncer.(*DefaultSyncer)
 	syncer.OnEventType("m.room.message", func(ev *Event) {
 		fmt.Println("Message: ", ev)
 	})
-	// To make the example non-blocking, call Sync() in a goroutine.
 	if err := cli.Sync(); err != nil {
 		fmt.Println("Sync() returned ", err)
 	}
+}
+
+func Example_nonBlockingSync() {
+	cli, _ := NewClient("https://matrix.org", "@example:matrix.org", "MDAefhiuwehfuiwe")
+	syncer := cli.Syncer.(*DefaultSyncer)
+	syncer.OnEventType("m.room.message", func(ev *Event) {
+		fmt.Println("Message: ", ev)
+	})
+	go func() {
+		for {
+			if err := cli.Sync(); err != nil {
+				fmt.Println("Sync() returned ", err)
+			}
+			// Optional: Wait a period of time before trying to sync again.
+		}
+	}()
 }
