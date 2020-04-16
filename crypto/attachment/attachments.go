ad28e1b46d6789d9819f19b568a642d87773a938
Tulir Asokan <tulir@maunium.net>
2020-04-16 16:51:41 +0300

    Add file encryption stuff

---
+++ crypto/attachment/attachments.go
@@ -0,0 +1,162 @@
+// Copyright (c) 2020 Tulir Asokan
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package attachment
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"math"
+)
+
+var (
+	HashMismatch         = errors.New("mismatching SHA-256 digest")
+	UnsupportedVersion   = errors.New("unsupported Matrix file encryption version")
+	UnsupportedAlgorithm = errors.New("unsupported JWK encryption algorithm")
+	InvalidKey           = errors.New("failed to decode key")
+	InvalidInitVector    = errors.New("failed to decode initialization vector")
+)
+
+const (
+	keyLength  = 32
+	hashLength = 32
+	ivLength   = 16
+)
+
+var (
+	keyBase64Length  = base64Length(keyLength)
+	hashBase64Length = base64Length(hashLength)
+	ivBase64Length   = base64Length(ivLength)
+)
+
+var unpaddedBase64 = base64.StdEncoding.WithPadding(base64.NoPadding)
+
+type JSONWebKey struct {
+	Key         string   `json:"k"`
+	Algorithm   string   `json:"alg"`
+	Extractable bool     `json:"ext"`
+	KeyType     string   `json:"kty"`
+	KeyOps      []string `json:"key_ops"`
+}
+
+type EncryptedFileHashes struct {
+	SHA256 string `json:"sha256"`
+}
+
+type decodedKeys struct {
+	key [keyLength]byte
+	iv  [ivLength]byte
+}
+
+type EncryptedFile struct {
+	Key        JSONWebKey          `json:"key"`
+	InitVector string              `json:"iv"`
+	Hashes     EncryptedFileHashes `json:"hashes"`
+	Version    string              `json:"v"`
+
+	decoded *decodedKeys `json:"-"`
+}
+
+func NewEncryptedFile() *EncryptedFile {
+	key, iv := genA256CTR()
+	return &EncryptedFile{
+		Key: JSONWebKey{
+			Key:         unpaddedBase64.EncodeToString(key[:]),
+			Algorithm:   "A256CTR",
+			Extractable: true,
+			KeyType:     "oct",
+			KeyOps:      []string{"encrypt", "decrypt"},
+		},
+		InitVector: unpaddedBase64.EncodeToString(iv[:]),
+		Version:    "v2",
+
+		decoded: &decodedKeys{key, iv},
+	}
+}
+
+
+func (ef *EncryptedFile) decodeKeys() error {
+	if ef.decoded != nil {
+		return nil
+	} else if len(ef.Key.Key) != keyBase64Length {
+		return InvalidKey
+	} else if len(ef.InitVector) != ivBase64Length {
+		return InvalidInitVector
+	}
+	ef.decoded = &decodedKeys{}
+	_, err := unpaddedBase64.Decode(ef.decoded.key[:], []byte(ef.Key.Key))
+	if err != nil {
+		return InvalidKey
+	}
+	_, err = unpaddedBase64.Decode(ef.decoded.iv[:], []byte(ef.InitVector))
+	if err != nil {
+		return InvalidInitVector
+	}
+	return nil
+}
+
+func (ef *EncryptedFile) Encrypt(plaintext []byte) []byte {
+	ef.decodeKeys()
+	ciphertext := xorA256CTR(plaintext, ef.decoded.key, ef.decoded.iv)
+	hash := sha256.Sum256(ciphertext)
+	ef.Hashes.SHA256 = unpaddedBase64.EncodeToString(hash[:])
+	return ciphertext
+}
+
+func (ef *EncryptedFile) checkHash(ciphertext []byte) bool {
+	if len(ef.Hashes.SHA256) != hashBase64Length {
+		return false
+	}
+	var hash [hashLength]byte
+	_, err := unpaddedBase64.Decode(hash[:], []byte(ef.Hashes.SHA256))
+	if err != nil {
+		return false
+	}
+	return hash == sha256.Sum256(ciphertext)
+}
+
+func (ef *EncryptedFile) Decrypt(ciphertext []byte) ([]byte, error) {
+	if ef.Version != "v2" {
+		return nil, UnsupportedVersion
+	} else if ef.Key.Algorithm != "A256CTR" {
+		return nil, UnsupportedAlgorithm
+	} else if !ef.checkHash(ciphertext) {
+		return nil, HashMismatch
+	} else if err := ef.decodeKeys(); err != nil {
+		return nil, err
+	} else {
+		return xorA256CTR(ciphertext, ef.decoded.key, ef.decoded.iv), nil
+	}
+}
+
+func base64Length(bytes int) int {
+	return int(math.Ceil(4 * (float64(bytes) / 3)))
+}
+
+func xorA256CTR(source []byte, key [keyLength]byte, iv [ivLength]byte) []byte {
+	block, _ := aes.NewCipher(key[:])
+	result := make([]byte, len(source))
+	cipher.NewCTR(block, iv[:]).XORKeyStream(result, source)
+	return result
+}
+
+func genA256CTR() (key [keyLength]byte, iv [ivLength]byte) {
+	_, err := rand.Read(key[:])
+	if err != nil {
+		panic(err)
+	}
+
+	// For some reason we leave the 8 last bytes empty even though AES256-CTR has a 16-byte block size.
+	_, err = rand.Read(iv[:8])
+	if err != nil {
+		panic(err)
+	}
+	return
+}
---
+++ crypto/attachment/attachments_test.go
@@ -0,0 +1,96 @@
+package attachment
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+const helloWorldCiphertext = ":6\xc7O1yR\x06\xe8\xcf]"
+const helloWorldRawFile = `{
+  "v": "v2",
+  "key": {
+    "kty": "oct",
+    "alg": "A256CTR",
+    "ext": true,
+    "k": "35XNdmWKOpn6UYS82Y83wEY8LagwQZHX2X0kAFW7sdg",
+    "key_ops": [
+      "encrypt",
+      "decrypt"
+    ]
+  },
+  "iv": "DOtPz8bC3qgAAAAAAAAAAA",
+  "hashes": {
+    "sha256": "rO+040ZhUxbpbmIS9GUuMSen4NPKFxMzqOUJeemM8mk"
+  }
+}`
+const random16Bytes = "\x85\xb4\x16/\xcaO\x1d\xe6\x7f\x95\xeb\xdb+g\x11\xb1"
+var random16BytesBase64 = unpaddedBase64.EncodeToString([]byte(random16Bytes))
+
+func parseHelloWorld() *EncryptedFile {
+	file := &EncryptedFile{}
+	_ = json.Unmarshal([]byte(helloWorldRawFile), file)
+	return file
+}
+
+func TestDecryptHelloWorld(t *testing.T) {
+	file := parseHelloWorld()
+	plaintext, err := file.Decrypt([]byte(helloWorldCiphertext))
+	if err != nil {
+		t.Errorf("Failed to decrypt file: %v", err)
+	} else if string(plaintext) != "hello world" {
+		t.Errorf("Unexpected decrypt output: %v", plaintext)
+	}
+}
+
+func TestEncryptHelloWorld(t *testing.T) {
+	file := parseHelloWorld()
+	ciphertext := file.Encrypt([]byte("hello world"))
+	if string(ciphertext) != helloWorldCiphertext {
+		t.Errorf("Unexpected encrypt output: %v", ciphertext)
+	}
+}
+
+func TestUnsupportedVersion(t *testing.T) {
+	file := parseHelloWorld()
+	file.Version = "foo"
+	_, err := file.Decrypt([]byte(helloWorldCiphertext))
+	if err != UnsupportedVersion {
+		t.Errorf("Didn't get expected UnsupportedVersion error: %v", err)
+	}
+}
+
+func TestUnsupportedAlgorithm(t *testing.T) {
+	file := parseHelloWorld()
+	file.Key.Algorithm = "bar"
+	_, err := file.Decrypt([]byte(helloWorldCiphertext))
+	if err != UnsupportedAlgorithm {
+		t.Errorf("Didn't get expected UnsupportedAlgorithm error: %v", err)
+	}
+}
+
+func TestHashMismatch(t *testing.T) {
+	file := parseHelloWorld()
+	file.Hashes.SHA256 = random16BytesBase64
+	_, err := file.Decrypt([]byte(helloWorldCiphertext))
+	if err != HashMismatch {
+		t.Errorf("Didn't get expected HashMismatch error: %v", err)
+	}
+}
+
+func TestTooLongHash(t *testing.T) {
+	file := parseHelloWorld()
+	file.Hashes.SHA256 = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVlciBhZGlwaXNjaW5nIGVsaXQuIFNlZCBwb3N1ZXJlIGludGVyZHVtIHNlbS4gUXVpc3F1ZSBsaWd1bGEgZXJvcyB1bGxhbWNvcnBlciBxdWlzLCBsYWNpbmlhIHF1aXMgZmFjaWxpc2lzIHNlZCBzYXBpZW4uCg"
+	_, err := file.Decrypt([]byte(helloWorldCiphertext))
+	if err != HashMismatch {
+		t.Errorf("Didn't get expected HashMismatch error: %v", err)
+	}
+}
+
+func TestTooShortHash(t *testing.T) {
+	file := parseHelloWorld()
+	file.Hashes.SHA256 = "5/Gy1JftyyQ"
+	_, err := file.Decrypt([]byte(helloWorldCiphertext))
+	if err != HashMismatch {
+		t.Errorf("Didn't get expected HashMismatch error: %v", err)
+	}
+}
