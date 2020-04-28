5961aff7a736568642562d0d4c0538ded30d37e8
Tulir Asokan <tulir@maunium.net>
2020-04-29 02:54:23 +0300

    Fix test

--- crypto/attachment/attachments_test.go
+++ crypto/attachment/attachments_test.go
@@ -1,6 +1,7 @@
 package attachment
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"testing"
 )
@@ -24,7 +25,7 @@ const helloWorldRawFile = `{
   }
 }`
 const random16Bytes = "\x85\xb4\x16/\xcaO\x1d\xe6\x7f\x95\xeb\xdb+g\x11\xb1"
-var random16BytesBase64 = unpaddedBase64.EncodeToString([]byte(random16Bytes))
+var random16BytesBase64 = base64.StdEncoding.EncodeToString([]byte(random16Bytes))
 
 func parseHelloWorld() *EncryptedFile {
 	file := &EncryptedFile{}
