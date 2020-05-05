25b5009a0cf081b8baa4ac5018220aaf2d820a0c
Tulir Asokan <tulir@maunium.net>
2020-05-05 16:15:32 +0300

    Update copyright headers in pushrule tests

--- pushrules/condition_eventmatch_test.go
+++ pushrules/condition_eventmatch_test.go
@@ -1,18 +1,8 @@
-// gomuks - A terminal Matrix client written in Go.
-// Copyright (C) 2020 Tulir Asokan
+// Copyright (c) 2020 Tulir Asokan
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package pushrules_test
 
