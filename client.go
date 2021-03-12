854ce484d7d8696ee0b31d734376d9eb8ac010ca
Brendan Abolivier <babolivier@matrix.org>
2021-03-12 16:06:12 +0100

    Implement helpers for fetching user presence status

--- client.go
+++ client.go
@@ -813,6 +813,19 @@ func (cli *Client) UserTyping(roomID id.RoomID, typing bool, timeout int64) (res
 	return
 }
 
+// GetPresence gets the presence of the user with the specified MXID. See https://matrix.org/docs/spec/client_server/latest#get-matrix-client-r0-presence-userid-status
+func (cli *Client) GetPresence(userID id.UserID) (resp *RespPresence, err error) {
+	resp = new(RespPresence)
+	u := cli.BuildURL("presence", userID, "status")
+	_, err = cli.MakeRequest("GET", u, nil, resp)
+	return
+}
+
+// GetPresence gets the user's presence. See https://matrix.org/docs/spec/client_server/latest#get-matrix-client-r0-presence-userid-status
+func (cli *Client) GetOwnPresence() (resp *RespPresence, err error) {
+	return cli.GetPresence(cli.UserID)
+}
+
 func (cli *Client) SetPresence(status event.Presence) (err error) {
 	req := ReqPresence{Presence: status}
 	u := cli.BuildURL("presence", cli.UserID, "status")
