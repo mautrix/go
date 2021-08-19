53be7e7ac804f28f25ef24f1822631c57bbfbdc9
Tulir Asokan <tulir@maunium.net>
2021-08-19 19:10:40 +0300

    Add crypto store upgrade for converting VARCHAR(255) to TEXT

--- crypto/sql_store_upgrade/upgrade.go
+++ crypto/sql_store_upgrade/upgrade.go
@@ -233,6 +233,49 @@ var Upgrades = [...]upgradeFunc{
 		}
 		return nil
 	},
+	func(tx *sql.Tx, dialect string) error {
+		if dialect == "sqlite3" {
+			// SQLite doesn't enforce varchar sizes anyway
+			return nil
+		}
+		alters := [...]string{
+			`ALTER TABLE crypto_account ALTER COLUMN device_id TYPE TEXT`,
+			`ALTER TABLE crypto_account ALTER COLUMN account_id TYPE TEXT`,
+
+			`ALTER TABLE crypto_device ALTER COLUMN user_id TYPE TEXT`,
+			`ALTER TABLE crypto_device ALTER COLUMN device_id TYPE TEXT`,
+			`ALTER TABLE crypto_device ALTER COLUMN name TYPE TEXT`,
+
+			`ALTER TABLE crypto_megolm_inbound_session ALTER COLUMN room_id TYPE TEXT`,
+			`ALTER TABLE crypto_megolm_inbound_session ALTER COLUMN account_id TYPE TEXT`,
+			`ALTER TABLE crypto_megolm_inbound_session ALTER COLUMN withheld_code TYPE TEXT`,
+
+			`ALTER TABLE crypto_megolm_outbound_session ALTER COLUMN room_id TYPE TEXT`,
+			`ALTER TABLE crypto_megolm_outbound_session ALTER COLUMN account_id TYPE TEXT`,
+
+			`ALTER TABLE crypto_message_index ALTER COLUMN event_id TYPE TEXT`,
+
+			`ALTER TABLE crypto_olm_session ALTER COLUMN account_id TYPE TEXT`,
+
+			`ALTER TABLE crypto_tracked_user ALTER COLUMN user_id TYPE TEXT`,
+
+			`ALTER TABLE crypto_cross_signing_keys ALTER COLUMN user_id TYPE TEXT`,
+			`ALTER TABLE crypto_cross_signing_keys ALTER COLUMN usage TYPE TEXT`,
+
+			`ALTER TABLE crypto_cross_signing_signatures ALTER COLUMN signed_user_id TYPE TEXT`,
+			`ALTER TABLE crypto_cross_signing_signatures ALTER COLUMN signed_key TYPE TEXT`,
+			`ALTER TABLE crypto_cross_signing_signatures ALTER COLUMN signer_user_id TYPE TEXT`,
+			`ALTER TABLE crypto_cross_signing_signatures ALTER COLUMN signer_key TYPE TEXT`,
+			`ALTER TABLE crypto_cross_signing_signatures ALTER COLUMN signature TYPE TEXT`,
+		}
+		for _, alter := range alters {
+			_, err := tx.Exec(alter)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	},
 }
 
 // GetVersion returns the current version of the DB schema.
