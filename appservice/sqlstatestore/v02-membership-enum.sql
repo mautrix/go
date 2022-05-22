87f89ec33247708a2d8b9c4ca6306d4d9c2b5372
Tulir Asokan <tulir@maunium.net>
2022-05-22 16:15:15 +0300

    Fix invalid membership values when converting to enum

--- appservice/sqlstatestore/v02-membership-enum.sql
+++ appservice/sqlstatestore/v02-membership-enum.sql
@@ -2,4 +2,5 @@
 -- only: postgres
 
 CREATE TYPE membership AS ENUM ('join', 'leave', 'invite', 'ban', 'knock');
+UPDATE mx_user_profile SET membership='leave' WHERE LOWER(membership) NOT IN ('join', 'leave', 'invite', 'ban', 'knock');
 ALTER TABLE mx_user_profile ALTER COLUMN membership TYPE membership USING LOWER(membership)::membership;
