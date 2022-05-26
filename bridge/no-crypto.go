cf90afd2498e60d747c0ee9c8e8c4429c0c61f2a
Tulir Asokan <tulir@maunium.net>
2022-05-26 14:30:01 +0300

    Fix building bridges without crypto

--- bridge/no-crypto.go
+++ bridge/no-crypto.go
@@ -1,16 +1,23 @@
+// Copyright (c) 2022 Tulir Asokan
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //go:build !cgo || nocrypto
 
-package main
+package bridge
 
 import (
 	"errors"
 )
 
 func NewCryptoHelper(bridge *Bridge) Crypto {
-	if !bridge.Config.Bridge.Encryption.Allow {
+	if bridge.Config.Bridge.GetEncryptionConfig().Allow {
 		bridge.Log.Warnln("Bridge built without end-to-bridge encryption, but encryption is enabled in config")
+	} else {
+		bridge.Log.Debugln("Bridge built without end-to-bridge encryption")
 	}
-	bridge.Log.Debugln("Bridge built without end-to-bridge encryption")
 	return nil
 }
 
