43e9bc0bdd59a4d271f05ccef81c004ba961a19a
Tulir Asokan <tulir@maunium.net>
2022-05-31 18:54:21 +0300

    Add missing letter. Fixes mautrix/whatsapp#509

--- appservice/sqlstatestore/v03-no-null.sql
+++ appservice/sqlstatestore/v03-no-null.sql
@@ -3,7 +3,7 @@
 UPDATE mx_user_profile SET displayname='' WHERE displayname IS NULL;
 UPDATE mx_user_profile SET avatar_url='' WHERE avatar_url IS NULL;
 
--- only postgres for next 4 lines
+-- only: postgres for next 4 lines
 ALTER TABLE mx_user_profile ALTER COLUMN displayname SET DEFAULT '';
 ALTER TABLE mx_user_profile ALTER COLUMN displayname SET NOT NULL;
 ALTER TABLE mx_user_profile ALTER COLUMN avatar_url SET DEFAULT '';
