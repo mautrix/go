29609b87cf59c8a054690e660f0dcb96622136f3
Tulir Asokan <tulir@maunium.net>
2022-11-08 10:50:47 +0200

    Add sqlite3-fk-wal mode matching litestream without disabling checkpointing

--- util/dbutil/database.go
+++ util/dbutil/database.go
@@ -40,7 +40,7 @@ func ParseDialect(engine string) (Dialect, error) {
 	switch strings.ToLower(engine) {
 	case "postgres", "postgresql":
 		return Postgres, nil
-	case "sqlite3", "sqlite", "litestream":
+	case "sqlite3", "sqlite", "litestream", "sqlite3-fk-wal":
 		return SQLite, nil
 	default:
 		return DialectUnknown, fmt.Errorf("unknown dialect '%s'", engine)
--- util/dbutil/litestream/register.go
+++ util/dbutil/litestream/register.go
@@ -31,4 +31,25 @@ func init() {
 			return
 		},
 	})
+
+	sql.Register("sqlite3-fk-wal", &sqlite3.SQLiteDriver{
+		ConnectHook: func(conn *sqlite3.SQLiteConn) (err error) {
+			if err = conn.SetFileControlInt("main", sqlite3.SQLITE_FCNTL_PERSIST_WAL, 1); err != nil {
+				return
+			}
+			if _, err = conn.Exec("PRAGMA foreign_keys = ON", []driver.Value{}); err != nil {
+				return
+			}
+			if _, err = conn.Exec("PRAGMA journal_mode = WAL", []driver.Value{}); err != nil {
+				return
+			}
+			if _, err = conn.Exec("PRAGMA synchronous = NORMAL", []driver.Value{}); err != nil {
+				return
+			}
+			if _, err = conn.Exec("PRAGMA busy_timeout = 5000", []driver.Value{}); err != nil {
+				return
+			}
+			return
+		},
+	})
 }
