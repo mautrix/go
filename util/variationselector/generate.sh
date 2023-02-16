147a7106ef802aa48e12274ebe147b63c21648f5
Tulir Asokan <tulir@maunium.net>
2023-02-16 12:25:55 +0200

    Update variation selector script emoji version

--- util/variationselector/generate.sh
+++ util/variationselector/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "$(
-	curl -s https://www.unicode.org/Public/14.0.0/ucd/emoji/emoji-variation-sequences.txt \
+	curl -s https://www.unicode.org/Public/15.0.0/ucd/emoji/emoji-variation-sequences.txt \
 	| grep FE0F \
 	| awk '{ printf("\\U%8s\n", $1) }' \
 	| sed 's/ /0/g'
