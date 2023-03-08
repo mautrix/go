07d6de5a8d42e4c146224a0129172752ccdd708f
Toni Spets <toni.spets@beeper.com>
2023-03-08 08:02:50 +0200

    Expose database errors in QueryTiming logs

--- util/dbutil/log.go
+++ util/dbutil/log.go
@@ -11,7 +11,7 @@ import (
 )
 
 type DatabaseLogger interface {
-	QueryTiming(ctx context.Context, method, query string, args []interface{}, nrows int, duration time.Duration)
+	QueryTiming(ctx context.Context, method, query string, args []interface{}, nrows int, duration time.Duration, err error)
 	WarnUnsupportedVersion(current, latest int)
 	PrepareUpgrade(current, latest int)
 	DoUpgrade(from, to int, message string, txn bool)
@@ -28,7 +28,7 @@ func (n noopLogger) PrepareUpgrade(_, _ int)              {}
 func (n noopLogger) DoUpgrade(_, _ int, _ string, _ bool) {}
 func (n noopLogger) Warn(msg string, args ...interface{}) {}
 
-func (n noopLogger) QueryTiming(_ context.Context, _, _ string, _ []interface{}, _ int, _ time.Duration) {
+func (n noopLogger) QueryTiming(_ context.Context, _, _ string, _ []interface{}, _ int, _ time.Duration, _ error) {
 }
 
 type mauLogger struct {
@@ -52,7 +52,7 @@ func (m mauLogger) DoUpgrade(from, to int, message string, _ bool) {
 	m.l.Infofln("Upgrading database from v%d to v%d: %s", from, to, message)
 }
 
-func (m mauLogger) QueryTiming(_ context.Context, method, query string, _ []interface{}, _ int, duration time.Duration) {
+func (m mauLogger) QueryTiming(_ context.Context, method, query string, _ []interface{}, _ int, duration time.Duration, _ error) {
 	if duration > 1*time.Second {
 		m.l.Warnfln("%s(%s) took %.3f seconds", method, query, duration.Seconds())
 	}
@@ -103,7 +103,7 @@ func (z zeroLogger) DoUpgrade(from, to int, message string, txn bool) {
 
 var whitespaceRegex = regexp.MustCompile(`\s+`)
 
-func (z zeroLogger) QueryTiming(ctx context.Context, method, query string, args []interface{}, nrows int, duration time.Duration) {
+func (z zeroLogger) QueryTiming(ctx context.Context, method, query string, args []interface{}, nrows int, duration time.Duration, err error) {
 	log := zerolog.Ctx(ctx)
 	if log.GetLevel() == zerolog.Disabled || log == zerolog.DefaultContextLogger {
 		log = z.l
@@ -117,6 +117,7 @@ func (z zeroLogger) QueryTiming(ctx context.Context, method, query string, args 
 	}
 	query = strings.TrimSpace(whitespaceRegex.ReplaceAllLiteralString(query, " "))
 	log.Trace().
+		Err(err).
 		Int64("duration_µs", duration.Microseconds()).
 		Str("method", method).
 		Str("query", query).
