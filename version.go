e464b2491034d5325a639ab7ab343d1ed0e6fb3a
Tulir Asokan <tulir@maunium.net>
2023-04-25 15:04:31 +0300

    Allow compiling commit hash into user agent

--- version.go
+++ version.go
@@ -1,5 +1,31 @@
 package mautrix
 
+import (
+	"fmt"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
 const Version = "v0.15.1"
 
-var DefaultUserAgent = "mautrix-go/" + Version
+var GoModVersion = ""
+var Commit = ""
+var VersionWithCommit = Version
+
+var DefaultUserAgent = "mautrix-go/" + Version + " go/" + strings.TrimPrefix(runtime.Version(), "go")
+
+var goModVersionRegex = regexp.MustCompile(`v.+\d{14}-([0-9a-f]{12})`)
+
+func init() {
+	if GoModVersion != "" {
+		match := goModVersionRegex.FindStringSubmatch(GoModVersion)
+		if match != nil {
+			Commit = match[1]
+		}
+	}
+	if Commit != "" {
+		VersionWithCommit = fmt.Sprintf("%s+dev.%s", Version, Commit[:8])
+		DefaultUserAgent = strings.Replace(DefaultUserAgent, "mautrix-go/"+Version, "mautrix-go/"+VersionWithCommit, 1)
+	}
+}
