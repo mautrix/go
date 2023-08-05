363fd935752d2eb65cfbdade33c8dbe62c5fafcb
Tulir Asokan <tulir@maunium.net>
2023-08-05 15:21:44 +0300

    Simplify and optimize RandomString

--- util/random.go
+++ util/random.go
@@ -8,7 +8,7 @@ package util
 
 import (
 	"crypto/rand"
-	"encoding/base64"
+	"encoding/binary"
 	"hash/crc32"
 	"strings"
 	"unsafe"
@@ -23,36 +23,35 @@ func RandomBytes(n int) []byte {
 	return data
 }
 
-var letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+// RandomStringBytes generates a random string of the given length and returns it as a byte array.
+func RandomStringBytes(n int) []byte {
+	if n <= 0 {
+		return []byte{}
+	}
+	input := RandomBytes(n * 2)
+	for i := 0; i < n; i++ {
+		// Risk of modulo bias is only 2 in 65535, values between 0 and 65533 are uniformly distributed
+		input[i] = letters[binary.BigEndian.Uint16(input[i*2:])%uint16(len(letters))]
+	}
+	input = input[:n]
+	return input
+}
 
 // RandomString generates a random string of the given length.
 func RandomString(n int) string {
 	if n <= 0 {
 		return ""
 	}
-	base64Len := n
-	if n%4 != 0 {
-		base64Len += 4 - (n % 4)
-	}
-	decodedLength := base64.RawStdEncoding.DecodedLen(base64Len)
-	output := make([]byte, base64Len)
-	base64.RawStdEncoding.Encode(output, RandomBytes(decodedLength))
-	for i, char := range output {
-		if char == '+' || char == '/' {
-			_, err := rand.Read(output[i : i+1])
-			if err != nil {
-				panic(err)
-			}
-			output[i] = letters[int(output[i])%len(letters)]
-		}
-	}
-	return (*(*string)(unsafe.Pointer(&output)))[:n]
+	str := RandomStringBytes(n)
+	return *(*string)(unsafe.Pointer(&str))
 }
 
 func base62Encode(val uint32, minWidth int) string {
 	var buf strings.Builder
 	for val > 0 {
-		buf.WriteByte(letters[val%62])
+		buf.WriteByte(letters[val%uint32(len(letters))])
 		val /= 62
 	}
 	return strings.Repeat("0", minWidth-buf.Len()) + buf.String()
--- util/random_test.go
+++ util/random_test.go
@@ -7,16 +7,26 @@
 package util_test
 
 import (
+	"regexp"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"maunium.net/go/mautrix/util"
 )
 
 func TestRandomString_Length(t *testing.T) {
 	for i := 0; i < 256; i++ {
-		assert.Len(t, util.RandomString(i), i)
+		require.Len(t, util.RandomString(i), i)
+	}
+}
+
+var randomStringRegex = regexp.MustCompile(`^[0-9A-Za-z]*$`)
+var randomTokenRegex = regexp.MustCompile(`^.+?_[0-9A-Za-z]*_[0-9A-Za-z]{6}$`)
+
+func TestRandomString_Content(t *testing.T) {
+	for i := 0; i < 256; i++ {
+		require.Regexp(t, randomStringRegex, util.RandomString(i))
 	}
 }
 
@@ -24,6 +34,38 @@ func TestRandomToken(t *testing.T) {
 	for i := 0; i < 256; i++ {
 		// Format: prefix_random_checksum
 		// Length: prefix (4) + 1 + random (i) + 1 + checksum (6)
-		assert.Len(t, util.RandomToken("meow", i), i+5+7)
+		token := util.RandomToken("meow", i)
+		require.Len(t, token, i+5+7)
+		require.Regexp(t, randomTokenRegex, token)
+	}
+}
+
+func BenchmarkRandomString8(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		util.RandomString(8)
+	}
+}
+
+func BenchmarkRandomString32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		util.RandomString(32)
+	}
+}
+
+func BenchmarkRandomString50(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		util.RandomString(50)
+	}
+}
+
+func BenchmarkRandomString256(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		util.RandomString(256)
+	}
+}
+
+func BenchmarkRandomToken32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		util.RandomToken("meow", 32)
 	}
 }
