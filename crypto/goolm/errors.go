ca03f1df17fc2f5cc46462f32fa359e44a5f521d
Tulir Asokan <tulir@maunium.net>
2023-12-15 14:17:49 +0200

    Remove unused error constants

--- crypto/goolm/errors.go
+++ crypto/goolm/errors.go
@@ -6,13 +6,11 @@ import (
 
 // Those are the most common used errors
 var (
-	ErrNoSigningKey         = errors.New("no signing key")
 	ErrBadSignature         = errors.New("bad signature")
 	ErrBadMAC               = errors.New("bad mac")
 	ErrBadMessageFormat     = errors.New("bad message format")
 	ErrBadVerification      = errors.New("bad verification")
 	ErrWrongProtocolVersion = errors.New("wrong protocol version")
-	ErrNoSessionKey         = errors.New("no session key")
 	ErrEmptyInput           = errors.New("empty input")
 	ErrNoKeyProvided        = errors.New("no key")
 	ErrBadMessageKeyID      = errors.New("bad message key id")
@@ -25,12 +23,8 @@ var (
 	ErrBadVersion           = errors.New("wrong version")
 	ErrNotBlocksize         = errors.New("length != blocksize")
 	ErrNotMultipleBlocksize = errors.New("length not a multiple of the blocksize")
-	ErrBase64InvalidLength  = errors.New("base64 decode invalid length")
-	ErrWrongPickleVersion   = errors.New("Wrong pickle version")
-	ErrSignatureNotFound    = errors.New("signature not found")
-	ErrNotEnoughGoRandom    = errors.New("Not enough random data available")
+	ErrWrongPickleVersion   = errors.New("wrong pickle version")
 	ErrValueTooShort        = errors.New("value too short")
 	ErrInputToSmall         = errors.New("input too small (truncated?)")
 	ErrOverflow             = errors.New("overflow")
-	ErrBadBase64            = errors.New("Bad base64")
 )
