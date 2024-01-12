066db534f683d5cc1290ec94635bb118c64c208f
Sumner Evans <sumner@beeper.com>
2024-01-11 18:52:07 -0700

    crypto/pkcs7: add package for (un)padding data using PKCS#7
    Signed-off-by: Sumner Evans <sumner@beeper.com>

---
+++ crypto/pkcs7/pkcs7.go
@@ -0,0 +1,24 @@
+package pkcs7
+
+import "bytes"
+
+// Pad implements PKCS#7 padding as defined in [RFC2315]. It pads the plaintext
+// to the given blockSize in the range [1, 255]. This is normally used in
+// AES-CBC encryption.
+//
+// [RFC2315]: https://www.ietf.org/rfc/rfc2315.txt
+func Pad(plaintext []byte, blockSize int) []byte {
+	padding := blockSize - len(plaintext)%blockSize
+	return append(plaintext, bytes.Repeat([]byte{byte(padding)}, padding)...)
+}
+
+// Unpad implements PKCS#7 unpadding as defined in [RFC2315]. It unpads the
+// plaintext by reading the padding amount from the last byte of the plaintext.
+// This is normally used in AES-CBC decryption.
+//
+// [RFC2315]: https://www.ietf.org/rfc/rfc2315.txt
+func Unpad(plaintext []byte) []byte {
+	length := len(plaintext)
+	unpadding := int(plaintext[length-1])
+	return plaintext[:length-unpadding]
+}
