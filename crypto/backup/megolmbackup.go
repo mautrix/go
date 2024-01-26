7d6b1143895398743a2ad98e4bfc73d244bbf766
Toni Spets <toni.spets@beeper.com>
2024-01-26 14:12:59 +0200

    Import group session signing key from backup

--- crypto/backup/megolmbackup.go
+++ crypto/backup/megolmbackup.go
@@ -15,15 +15,19 @@ type MegolmAuthData struct {
 	Signatures signatures.Signatures `json:"signatures"`
 }
 
+type SenderClaimedKeys struct {
+	Ed25519 id.Ed25519 `json:"ed25519"`
+}
+
 // MegolmSessionData is the decrypted session_data when the key backup is created
 // with the [id.KeyBackupAlgorithmMegolmBackupV1] algorithm as defined in
 // [Section 11.12.3.2.2 of the Spec].
 //
 // [Section 11.12.3.2.2 of the Spec]: https://spec.matrix.org/v1.9/client-server-api/#backup-algorithm-mmegolm_backupv1curve25519-aes-sha2
 type MegolmSessionData struct {
-	Algorithm          id.Algorithm               `json:"algorithm"`
-	ForwardingKeyChain []string                   `json:"forwarding_curve25519_key_chain"`
-	SenderClaimedKeys  map[id.KeyAlgorithm]string `json:"sender_claimed_keys"`
-	SenderKey          id.SenderKey               `json:"sender_key"`
-	SessionKey         []byte                     `json:"session_key"`
+	Algorithm          id.Algorithm      `json:"algorithm"`
+	ForwardingKeyChain []string          `json:"forwarding_curve25519_key_chain"`
+	SenderClaimedKeys  SenderClaimedKeys `json:"sender_claimed_keys"`
+	SenderKey          id.SenderKey      `json:"sender_key"`
+	SessionKey         []byte            `json:"session_key"`
 }
