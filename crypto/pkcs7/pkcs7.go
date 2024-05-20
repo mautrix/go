dd1dfb9bab27a403e312bde65deb272a23585ca8
Sumner Evans <sumner.evans@automattic.com>
2024-05-20 10:57:51 -0600

    pkcs7: update parameter names and documentation
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- crypto/pkcs7/pkcs7.go
+++ crypto/pkcs7/pkcs7.go
@@ -8,23 +8,23 @@ package pkcs7
 
 import "bytes"
 
-// Pad implements PKCS#7 padding as defined in [RFC2315]. It pads the plaintext
-// to the given blockSize in the range [1, 255]. This is normally used in
-// AES-CBC encryption.
+// Pad implements PKCS#7 padding as defined in [RFC2315]. It pads the data to
+// the given blockSize in the range [1, 255]. This is normally used in AES-CBC
+// encryption.
 //
 // [RFC2315]: https://www.ietf.org/rfc/rfc2315.txt
-func Pad(plaintext []byte, blockSize int) []byte {
-	padding := blockSize - len(plaintext)%blockSize
-	return append(plaintext, bytes.Repeat([]byte{byte(padding)}, padding)...)
+func Pad(data []byte, blockSize int) []byte {
+	padding := blockSize - len(data)%blockSize
+	return append(data, bytes.Repeat([]byte{byte(padding)}, padding)...)
 }
 
 // Unpad implements PKCS#7 unpadding as defined in [RFC2315]. It unpads the
-// plaintext by reading the padding amount from the last byte of the plaintext.
-// This is normally used in AES-CBC decryption.
+// data by reading the padding amount from the last byte of the data. This is
+// normally used in AES-CBC decryption.
 //
 // [RFC2315]: https://www.ietf.org/rfc/rfc2315.txt
-func Unpad(plaintext []byte) []byte {
-	length := len(plaintext)
-	unpadding := int(plaintext[length-1])
-	return plaintext[:length-unpadding]
+func Unpad(data []byte) []byte {
+	length := len(data)
+	unpadding := int(data[length-1])
+	return data[:length-unpadding]
 }
