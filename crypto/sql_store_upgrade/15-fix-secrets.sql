e0720029620fb01e5b545aa22d4e2bd48a1d259f
Tulir Asokan <tulir@maunium.net>
2024-05-27 14:30:35 +0300

    Fill account_id when updating crypto_secrets schema

--- crypto/sql_store_upgrade/15-fix-secrets.sql
+++ crypto/sql_store_upgrade/15-fix-secrets.sql
@@ -14,3 +14,8 @@ FROM crypto_secrets;
 DROP TABLE crypto_secrets;
 
 ALTER TABLE crypto_secrets_new RENAME TO crypto_secrets;
+
+-- only: sqlite
+UPDATE crypto_secrets SET account_id=(SELECT account_id FROM crypto_account ORDER BY rowid DESC LIMIT 1);
+-- only: postgres
+UPDATE crypto_secrets SET account_id=(SELECT account_id FROM crypto_account LIMIT 1);
