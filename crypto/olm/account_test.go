e3b8a55e2264dff14746837b1a8737eceff3f6ad
Sumner Evans <sumner.evans@automattic.com>
2024-06-21 11:06:06 -0600

    olm/session: add test for encrypt/decrypt
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- crypto/olm/account_test.go
+++ crypto/olm/account_test.go
@@ -18,14 +18,38 @@ import (
 	"maunium.net/go/mautrix/crypto/ed25519"
 	"maunium.net/go/mautrix/crypto/goolm/account"
 	"maunium.net/go/mautrix/crypto/libolm"
+	"maunium.net/go/mautrix/crypto/olm"
 )
+
+func ensureAccountsEqual(t *testing.T, a, b olm.Account) {
+	t.Helper()
+
+	assert.Equal(t, a.MaxNumberOfOneTimeKeys(), b.MaxNumberOfOneTimeKeys())
+
+	aEd25519, aCurve25519, err := a.IdentityKeys()
+	require.NoError(t, err)
+	bEd25519, bCurve25519, err := b.IdentityKeys()
+	require.NoError(t, err)
+	assert.Equal(t, aEd25519, bEd25519)
+	assert.Equal(t, aCurve25519, bCurve25519)
+
+	aIdentityKeysJSON, err := a.IdentityKeysJSON()
+	require.NoError(t, err)
+	bIdentityKeysJSON, err := b.IdentityKeysJSON()
+	require.NoError(t, err)
+	assert.JSONEq(t, string(aIdentityKeysJSON), string(bIdentityKeysJSON))
+
+	aOTKs, err := a.OneTimeKeys()
+	require.NoError(t, err)
+	bOTKs, err := b.OneTimeKeys()
+	require.NoError(t, err)
+	assert.Equal(t, aOTKs, bOTKs)
+}
 
 // TestAccount_UnpickleLibolmToGoolm tests creating an account from libolm,
 // pickling it, and importing it into goolm.
 func TestAccount_UnpickleLibolmToGoolm(t *testing.T) {
 	libolmAccount, err := libolm.NewAccount(nil)
-	require.NoError(t, err)
-	libolmEd25519, libolmCurve25519, err := libolmAccount.IdentityKeys()
 	require.NoError(t, err)
 
 	require.NoError(t, libolmAccount.GenOneTimeKeys(nil, 50))
@@ -35,23 +59,8 @@ func TestAccount_UnpickleLibolmToGoolm(t *testing.T) {
 
 	goolmAccount, err := account.AccountFromPickled(libolmPickled, []byte("test"))
 	require.NoError(t, err)
-	goolmEd25519, goolmCurve25519, err := goolmAccount.IdentityKeys()
-	require.NoError(t, err)
 
-	assert.Equal(t, libolmEd25519, goolmEd25519)
-	assert.Equal(t, libolmCurve25519, goolmCurve25519)
-
-	libolmIdentityKeysJSON, err := libolmAccount.IdentityKeysJSON()
-	require.NoError(t, err)
-	goolmIdentityKeysJSON, err := goolmAccount.IdentityKeysJSON()
-	require.NoError(t, err)
-	assert.JSONEq(t, string(libolmIdentityKeysJSON), string(goolmIdentityKeysJSON))
-
-	libolmOTKs, err := libolmAccount.OneTimeKeys()
-	require.NoError(t, err)
-	goolmOTKs, err := goolmAccount.OneTimeKeys()
-	require.NoError(t, err)
-	assert.Equal(t, libolmOTKs, goolmOTKs)
+	ensureAccountsEqual(t, libolmAccount, goolmAccount)
 
 	goolmPickled, err := goolmAccount.Pickle([]byte("test"))
 	require.NoError(t, err)
@@ -63,8 +72,6 @@ func TestAccount_UnpickleLibolmToGoolm(t *testing.T) {
 func TestAccount_UnpickleGoolmToLibolm(t *testing.T) {
 	goolmAccount, err := account.NewAccount(nil)
 	require.NoError(t, err)
-	goolmEd25519, goolmCurve25519, err := goolmAccount.IdentityKeys()
-	require.NoError(t, err)
 
 	require.NoError(t, goolmAccount.GenOneTimeKeys(nil, 50))
 
@@ -73,23 +80,8 @@ func TestAccount_UnpickleGoolmToLibolm(t *testing.T) {
 
 	libolmAccount, err := libolm.AccountFromPickled(bytes.Clone(goolmPickled), []byte("test"))
 	require.NoError(t, err)
-	libolmEd25519, libolmCurve25519, err := libolmAccount.IdentityKeys()
-	require.NoError(t, err)
 
-	assert.Equal(t, libolmEd25519, goolmEd25519)
-	assert.Equal(t, libolmCurve25519, goolmCurve25519)
-
-	libolmIdentityKeysJSON, err := libolmAccount.IdentityKeysJSON()
-	require.NoError(t, err)
-	goolmIdentityKeysJSON, err := goolmAccount.IdentityKeysJSON()
-	require.NoError(t, err)
-	assert.JSONEq(t, string(libolmIdentityKeysJSON), string(goolmIdentityKeysJSON))
-
-	libolmOTKs, err := libolmAccount.OneTimeKeys()
-	require.NoError(t, err)
-	goolmOTKs, err := goolmAccount.OneTimeKeys()
-	require.NoError(t, err)
-	assert.Equal(t, libolmOTKs, goolmOTKs)
+	ensureAccountsEqual(t, libolmAccount, goolmAccount)
 
 	libolmPickled, err := libolmAccount.Pickle([]byte("test"))
 	require.NoError(t, err)
@@ -111,8 +103,8 @@ func FuzzAccount_Sign(f *testing.F) {
 		require.NoError(t, err)
 		goolmSignature, err := goolmAccount.Sign(bytes.Clone(message))
 		require.NoError(t, err)
-
 		assert.Equal(t, goolmSignature, libolmSignature)
+
 		goolmSignatureBytes, err := base64.RawStdEncoding.DecodeString(string(goolmSignature))
 		require.NoError(t, err)
 		libolmSignatureBytes, err := base64.RawStdEncoding.DecodeString(string(libolmSignature))
