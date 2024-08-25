57565a9526461b21c2f06df01593960c83fb6615
Brad Murray <brad@beeper.com>
2024-08-25 13:21:22 -0400

    Add com.beeper.to_device sync filter field

--- filter.go
+++ filter.go
@@ -24,6 +24,7 @@ type Filter struct {
 	EventFormat EventFormat `json:"event_format,omitempty"`
 	Presence    FilterPart  `json:"presence,omitempty"`
 	Room        RoomFilter  `json:"room,omitempty"`
+	ToDevice    *FilterPart `json:"com.beeper.to_device,omitempty"`
 }
 
 // RoomFilter is used to define filtering rules for room events
