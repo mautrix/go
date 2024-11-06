ff907f403334e75fa2df1a733c9b904c6d2c6ea8
Tulir Asokan <tulir@maunium.net>
2024-11-06 09:26:59 +0100

    event/reply: implement MSC2781

--- event/reply.go
+++ event/reply.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Tulir Asokan
+// Copyright (c) 2024 Tulir Asokan
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,7 +7,6 @@
 package event
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 
@@ -47,52 +46,6 @@ func (content *MessageEventContent) GetReplyTo() id.EventID {
 	return content.RelatesTo.GetReplyTo()
 }
 
-const ReplyFormat = `<mx-reply><blockquote><a href="https://matrix.to/#/%s/%s">In reply to</a> <a href="https://matrix.to/#/%s">%s</a><br>%s</blockquote></mx-reply>`
-
-func (evt *Event) GenerateReplyFallbackHTML() string {
-	parsedContent, ok := evt.Content.Parsed.(*MessageEventContent)
-	if !ok {
-		return ""
-	}
-	parsedContent.RemoveReplyFallback()
-	body := parsedContent.FormattedBody
-	if len(body) == 0 {
-		body = TextToHTML(parsedContent.Body)
-	}
-
-	senderDisplayName := evt.Sender
-
-	return fmt.Sprintf(ReplyFormat, evt.RoomID, evt.ID, evt.Sender, senderDisplayName, body)
-}
-
-func (evt *Event) GenerateReplyFallbackText() string {
-	parsedContent, ok := evt.Content.Parsed.(*MessageEventContent)
-	if !ok {
-		return ""
-	}
-	parsedContent.RemoveReplyFallback()
-	body := parsedContent.Body
-	lines := strings.Split(strings.TrimSpace(body), "\n")
-	firstLine, lines := lines[0], lines[1:]
-
-	senderDisplayName := evt.Sender
-
-	var fallbackText strings.Builder
-	_, _ = fmt.Fprintf(&fallbackText, "> <%s> %s", senderDisplayName, firstLine)
-	for _, line := range lines {
-		_, _ = fmt.Fprintf(&fallbackText, "\n> %s", line)
-	}
-	fallbackText.WriteString("\n\n")
-	return fallbackText.String()
-}
-
 func (content *MessageEventContent) SetReply(inReplyTo *Event) {
 	content.RelatesTo = (&RelatesTo{}).SetReplyTo(inReplyTo.ID)
-
-	if content.MsgType == MsgText || content.MsgType == MsgNotice {
-		content.EnsureHasHTML()
-		content.FormattedBody = inReplyTo.GenerateReplyFallbackHTML() + content.FormattedBody
-		content.Body = inReplyTo.GenerateReplyFallbackText() + content.Body
-		content.replyFallbackRemoved = false
-	}
 }
