2ece158797577fd13903637b6b72f14162027d54
BurgerThe <226083023+BurgerThe@users.noreply.github.com>
2026-01-08 21:27:13 +0530

    Allow bots to take input

--- commands/handler.go
+++ commands/handler.go
@@ -7,62 +7,72 @@
 package commands
 
 import (
-	"strings"
-
-	"maunium.net/go/mautrix/event"
+        "strings"
+        "maunium.net/mautrix/go/event"
 )
 
 type Handler[MetaType any] struct {
-	// Func is the function that is called when the command is executed.
-	Func func(ce *Event[MetaType])
+        // Func is the function that is called when the command is executed.
+        Func func(ce *Event[MetaType])
 
-	// Name is the primary name of the command. It must be lowercase.
-	Name string
-	// Aliases are alternative names for the command. They must be lowercase.
-	Aliases []string
-	// Description is a description of the command.
-	Description *event.ExtensibleTextContainer
-	// Subcommands are subcommands of this command.
-	Subcommands []*Handler[MetaType]
-	// PreFunc is a function that is called before checking subcommands.
-	// It can be used to have parameters between subcommands (e.g. `!rooms <room ID> <command>`).
-	// Event.ShiftArg will likely be useful for implementing such parameters.
-	PreFunc func(ce *Event[MetaType])
-	// Parameters are the parameters of the command. These are used to suggest auto-completions to clients,
-	// but are not actually functional in any regard.
-	Parameters []*event.MSC4391Parameter
+        // Name is the primary name of the command. It must be lowercase.
+        Name string
+        // Aliases are alternative names for the command. They must be lowercase.
+        Aliases []string
+        // Subcommands are subcommands of this command.
+        Subcommands []*Handler[MetaType]
+        // PreFunc is a function that is called before checking subcommands.
+        // It can be used to have parameters between subcommands (e.g. `!rooms <room ID> <command>`).
+        // Event.ShiftArg will likely be useful for implementing such parameters.
+        PreFunc func(ce *Event[MetaType])
 
-	subcommandContainer *CommandContainer[MetaType]
+        //Param are parameters for each command.
+        Params []*event.MSC4391.Parameter
+
+        //This is the command syntax itself
+        BCommand []*event.MSC4391.BotCommand
+
+        //This is for input
+        Input []*event.MSC4332.BotCommandInput
+
+        subcommandContainer *CommandContainer[MetaType]
+}
+
+func (h *Handler[MetaType]) MakeMSC4391Event() *event.MSC4391BotCommandEventContent {
+        return &event.MSC4391BotCommandEventContent{
+                Command:     h.Name,
+                Aliases:     h.Aliases,
+                Description: h.Description,
+                Parameters:  h.Parameters,
+        }
 }
 
 func (h *Handler[MetaType]) initSubcommandContainer() {
-	if len(h.Subcommands) > 0 {
-		h.subcommandContainer = NewCommandContainer[MetaType]()
-		h.subcommandContainer.Register(h.Subcommands...)
-	} else {
-		h.subcommandContainer = nil
-	}
-}
-
-// MakeMSC4391Event creates a *event.MSC4391BotCommandEventContent representing this command handler.
-func (h *Handler[MetaType]) MakeMSC4391Event() *event.MSC4391BotCommandEventContent {
-	return &event.MSC4391BotCommandEventContent{
-		Command:     h.Name,
-		Aliases:     h.Aliases,
-		Description: h.Description,
-		Parameters:  h.Parameters,
-	}
+        if len(h.Subcommands) > 0 {
+                h.subcommandContainer = NewCommandContainer[MetaType]()
+                h.subcommandContainer.Register(h.Subcommands...)
+        } else {
+                h.subcommandContainer = nil
+        }
 }
 
 func MakeUnknownCommandHandler[MetaType any](prefix string) *Handler[MetaType] {
-	return &Handler[MetaType]{
-		Name: UnknownCommandName,
-		Func: func(ce *Event[MetaType]) {
-			if len(ce.ParentCommands) == 0 {
-				ce.Reply("Unknown command `%s%s`", prefix, ce.Command)
-			} else {
-				ce.Reply("Unknown subcommand `%s%s %s`", prefix, strings.Join(ce.ParentCommands, " "), ce.Command)
-			}
-		},
-	}
+        return &Handler[MetaType]{
+                Name: UnknownCommandName,
+                Func: func(ce *Event[MetaType]) {
+                        if len(ce.ParentCommands) == 0 {
+                                ce.Reply("Unknown command `%s%s`", prefix, ce.Command)
+                        } else {
+                                ce.Reply("Unknown subcommand `%s%s %s`", prefix, strings.Join(ce.ParentCommands, " "), ce.Command)
+                        }
+                },
+        }
+}
+
+func HandleCommands(cmd *Handler[MetaType])(prefix string) command(){
+        if(Input.Syntax == Command){
+                return &Handler[MetaType].ce.Command
+        } else {
+			MakeUnknownCommandHandler(Input.Syntax)
+		}
 }
