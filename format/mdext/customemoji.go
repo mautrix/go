bc0db9e5d16022fa51d50213e6746552f80b9105
SpiritCroc <dev@spiritcroc.de>
2026-04-30 17:42:52 +0200

    Keep it in a var

--- format/mdext/customemoji.go
+++ format/mdext/customemoji.go
@@ -60,10 +60,11 @@ func (cer *customEmojiRenderer) RegisterFuncs(reg renderer.NodeRendererFuncRegis
 }
 
 var emojiPrefix = []byte("Emoji: ")
+var uriSchemeSeparator = []byte("://")
 
 func (cer *customEmojiRenderer) renderImage(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
 	n, ok := node.(*ast.Image)
-	if ok && entering && bytes.HasPrefix(n.Title, emojiPrefix) && bytes.Contains(n.Destination, []byte("://")) {
+	if ok && entering && bytes.HasPrefix(n.Title, emojiPrefix) && bytes.Contains(n.Destination, uriSchemeSeparator) {
 		n.Title = bytes.TrimPrefix(n.Title, emojiPrefix)
 		n.SetAttributeString("data-mx-emoticon", nil)
 		n.SetAttributeString("height", "32")
