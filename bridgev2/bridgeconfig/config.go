82b412b48b8cdc5bc336709c9eccc3c8888e215d
Nick Mills-Barrett <nick@beeper.com>
2026-07-29 13:32:05 +0100

    bridgev2: add configurable debounce for transient connection states

--- bridgev2/bridgeconfig/config.go
+++ bridgev2/bridgeconfig/config.go
@@ -73,6 +73,7 @@ type BridgeConfig struct {
 	ResendBridgeInfo              bool               `yaml:"resend_bridge_info"`
 	NoBridgeInfoStateKey          bool               `yaml:"no_bridge_info_state_key"`
 	BridgeStatusNotices           string             `yaml:"bridge_status_notices"`
+	TransientStateDebounce        time.Duration      `yaml:"transient_state_debounce"`
 	UnknownErrorAutoReconnect     time.Duration      `yaml:"unknown_error_auto_reconnect"`
 	UnknownErrorMaxAutoReconnects int                `yaml:"unknown_error_max_auto_reconnects"`
 	BridgeMatrixLeave             bool               `yaml:"bridge_matrix_leave"`
