479c9e14a37ed5965ea66a2f668918cbce235a5b
Tulir Asokan <tulir@maunium.net>
2026-09-08 13:16:21 +0300

    Don't reconcile profile if member has a nickname

--- bridgev2/portal.go
+++ bridgev2/portal.go
@@ -4828,7 +4828,7 @@ func (portal *Portal) syncParticipants(
 				zerolog.Ctx(ctx).Err(err).Str("ghost_id", string(member.Sender)).Msg("Failed to get ghost from member list to update info")
 			} else {
 				ghost.UpdateInfo(ctx, member.UserInfo)
-				if current, ok := currentMembers[ghost.Intent.GetMXID()]; ok && current.Membership == event.MembershipJoin {
+				if current, ok := currentMembers[ghost.Intent.GetMXID()]; ok && current.Membership == event.MembershipJoin && ptr.Val(member.Nickname) == "" {
 					ghost.reconcileProfile(ctx, current)
 				}
 			}
